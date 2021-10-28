e822f81c3563f7694b1e911f7d601bdc47bd6e6b
muhaiminmdfatih <82874236+muhaiminmdfatih@users.noreply.github.com>
2021-10-28 17:56:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-noseX=0;
-noseY=0;
-function preload(){
-clown_nose=loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
-}
+function preload(){}
 
 function setup(){
 canvas=createCanvas(300,300);
@@ -16,19 +12,13 @@ poseNet.on('pose',gotPoses)
 function gotPoses(results){
 if(results.length>0){
 console.log(results);
-noseX=results[0].pose.nose.x
-noseY=results[0].pose.nose.y+5
-console.log("nose x= "+noseX);
-console.log("nose y= "+noseY);
+console.log("nose x= "+results[0].pose.nose.x);
+console.log("nose y= "+results[0].pose.nose.y);
 }
 }
 
 function draw(){
 image(video,0,0,300,300);
-circle(noseX,noseY,20)
-fill(255,0,0)
-stroke(255,0,0)
-image(clown_nose,noseX,noseY,30,30)
 }
 
 function take_snapshot(){
